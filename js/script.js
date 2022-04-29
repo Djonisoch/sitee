141b3687371afd8f2c1df41131d2d34ff7904eed
Djonisoch <104569528+Djonisoch@users.noreply.github.com>
2022-04-29 23:47:17 +0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,3 @@
-//   $(window).scroll(function() {
-//     if ($(this).scrollTop() > 10){
-//         $('header').addClass("header__filled"); 
-//          }
-//         else{
-//         $('header').removeClass("header__filled");         
-//       } 
-// });
-
-
 const header = document.querySelector(".header");
 window.addEventListener("scroll", checkScroll);
 document.addEventListener("DOMContentLoaded", checkScroll);
@@ -22,15 +12,4 @@ function checkScroll() {
   header.classList.remove("header__filled");
     }      
 }
-
-
-// $(document).ready(function() {
-//     var headerOffset = document.querySelector(".header").clientHeight;
-//     $("a.scrollto").click(function () {
-//       elementClick = $(this).attr("href")
-//       destination = $(elementClick).offset().top;
-//       $("html:not(:animated),body:not(:animated)").animate({scrollTop: destination - headerOffset}, 400);
-//       return false;
-//     });
-//   });
 
